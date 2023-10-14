b8ffc32b2dba3b2dd939c279187da7886d66d631
Nolan J Tait <nolanjtait@gmail.com>
2023-10-14 12:42:14 -0700

    Adds better comments to keymaps file

--- config/nvim/lua/core/keymaps.lua
+++ config/nvim/lua/core/keymaps.lua
@@ -1,17 +1,25 @@
 -- Sets up keybinds
 
-local keybinds = require("globals.keybinds")
-local options = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
+local options = {
+  -- Ensures that your mappings behave consistently and don't trigger unintended
+  -- mappings. When you use noremap in a mapping definition, it tells Vim not
+  -- to recursively map the right-hand side of the mapping.
+  noremap = true,
+  -- suppresses the display of most error messages and the displaying of the
+  -- command executed. This is helpful to prevent error messages or status
+  -- messages from appearing in the command line when the mapping is executed.
+  silent = true
+}
 
 -- Unbind ?
 keymap("", "?", "<Nop>", options)
 -- Rebind :W to :w to make it easier to save files when fat fingering
 keymap("c", "W", "w", options)
 
--- Sets up keymaps according to keybinds file
+-- Sets up keymaps according to globals/keybinds.lua file
+local keybinds = require("globals.keybinds")
+
 for _, keybind in ipairs(keybinds) do
   keymap(keybind.mode, keybind.key, keybind.command, options)
 end
-
-
