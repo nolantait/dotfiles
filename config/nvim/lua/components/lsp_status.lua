f2f3939e0308dd5360e915ea1e94bad99fb51313
Nolan J Tait <nolanjtait@gmail.com>
2023-10-14 18:58:09 -0700

    Rewrites lsp_status component to use modern api

--- config/nvim/lua/components/lsp_status.lua
+++ config/nvim/lua/components/lsp_status.lua
@@ -1,22 +1,11 @@
 return function()
-  local buf_ft = vim.api.nvim_get_option_value("filetype", { scope = "local" })
-  local clients = vim.lsp.get_active_clients()
-  local lsp_lists = {}
-  local available_servers = {}
-  if next(clients) == nil then
-    return ""  -- No server available
-  end
+  -- local buf_ft = vim.api.nvim_get_option_value("filetype", { scope = "local" })
+  local bufnr = vim.api.nvim_get_current_buf()
+  local clients = vim.lsp.get_clients({ bufnr = bufnr })
+
   for _, client in ipairs(clients) do
-    local filetypes = client.config.filetypes
-    local client_name = client.name
-    if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-      -- Avoid adding servers that already exists.
-      if not lsp_lists[client_name] then
-        lsp_lists[client_name] = true
-        table.insert(available_servers, client_name)
-      end
-    end
+    return string.format("%s %s", "", client.name)
   end
-  return next(available_servers) == nil and ""
-      or string.format("%s %s", "", table.concat(available_servers, ", "))
+
+  return ""  -- No server available
 end
