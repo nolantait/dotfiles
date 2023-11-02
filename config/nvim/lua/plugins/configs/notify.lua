f59c20f4393b8e44f684e418fa5ed769338ffad4
Nolan J Tait <nolanjtait@gmail.com>
2023-11-02 11:15:53 -0700

    Removes base rubocop config and tweaks notifications

--- config/nvim/lua/plugins/configs/notify.lua
+++ config/nvim/lua/plugins/configs/notify.lua
@@ -7,7 +7,7 @@ return function()
 
   notify.setup({
     ---@usage Animation style one of { "fade", "slide", "fade_in_slide_out", "static" }
-    stages = "fade_in_slide_out",
+    stages = "fade",
     ---@usage Function called when a new window is opened, use for changing win settings/config
     on_open = function(win)
       -- vim.api.nvim_set_option_value("winblend", 0, { scope = "local", win = win })
@@ -18,13 +18,13 @@ return function()
     ---@usage timeout for notifications in ms, default 5000
     timeout = 3000,
     -- @usage User render fps value
-    fps = 60,
+    fps = 20,
     -- Render function for notifications. See notify-render()
     render = "default",
     ---@usage highlight behind the window for stages that change opacity
     background_colour = colors.darker_gray,
     ---@usage minimum width for notification windows
-    minimum_width = 25,
+    minimum_width = 10,
     ---@usage notifications with level lower than this would be ignored. [ERROR > WARN > INFO > DEBUG > TRACE]
     level = "TRACE",
     ---@usage Icons for the different levels
