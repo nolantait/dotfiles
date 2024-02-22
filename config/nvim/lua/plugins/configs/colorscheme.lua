8b3dbcdd40780b02b7cc02b1fde2f005f35b159d
Nolan J Tait <nolanjtait@gmail.com>
2024-02-22 09:40:41 -0800

    Refactors colorscheme

--- config/nvim/lua/plugins/configs/colorscheme.lua
+++ config/nvim/lua/plugins/configs/colorscheme.lua
@@ -29,40 +29,30 @@ return function()
 
   local set_hl = vim.api.nvim_set_hl
 
-  set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
-
-  set_hl(0, "Structure", { fg = colors.yellow })
-  set_hl(0, "SpecialChar", { fg = colors.purple })
-
-  set_hl(0, "LineNr", { bg = colors.black, fg = colors.light_gray })
-  set_hl(0, "LineNrAbove", { bg = colors.black, fg = colors.light_gray })
-  set_hl(0, "LineNrBelow", { bg = colors.black, fg = colors.light_gray })
-  set_hl(0, "SignColumn", { bg = colors.black })
-
-  set_hl(0, "NormalFloat", { bg = colors.black })
-  set_hl(0, "FloatBorder", { fg = colors.light_gray })
-  set_hl(0, "FloatBorderCmp", { fg = colors.light_gray })
-  set_hl(0, "TelescopeBorder", { fg = colors.lighter_gray })
-  set_hl(0, "FloatBorderDocs", { fg = colors.blue })
-
-  set_hl(0, "MiniIndentscopeSymbol", { fg = colors.lighter_gray })
-  set_hl(0, "MiniMapSymbolLine", { fg = colors.white })
-  set_hl(0, "MiniMapSymbolView", { fg = colors.light_gray })
-
-  set_hl(0, "CmpPmenu", { bg = colors.black })
-
   set_hl(0, "AlphaFooter", { fg = colors.yellow })
   set_hl(0, "AlphaHeader", { fg = colors.blue })
-
-  set_hl(0, "MatchParen", { fg = colors.lightest_gray, bg = colors.light_gray, bold = true })
-
+  set_hl(0, "BufferLineFill", { bg = colors.background, fg = colors.background })
+  set_hl(0, "CmpPmenu", { bg = colors.black })
+  set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
+  set_hl(0, "FloatBorder", { fg = colors.light_gray })
+  set_hl(0, "FloatBorderCmp", { fg = colors.light_gray })
+  set_hl(0, "FloatBorderDocs", { fg = colors.blue })
   set_hl(0, "GitSignsAdd", { fg = colors.green })
   set_hl(0, "GitSignsAddNr", { fg = colors.green })
   set_hl(0, "GitSignsChange", { fg = colors.orange })
   set_hl(0, "GitSignsChangeNr", { fg = colors.orange })
   set_hl(0, "GitSignsDelete", { fg = colors.red })
   set_hl(0, "GitSignsDeleteNr", { fg = colors.red })
-
-  -- TODO: Figure out how to set this better
-  set_hl(0, "BufferLineFill", { bg = "#16181c", fg = "#16181c" })
+  set_hl(0, "LineNr", { bg = colors.black, fg = colors.light_gray })
+  set_hl(0, "LineNrAbove", { bg = colors.black, fg = colors.light_gray })
+  set_hl(0, "LineNrBelow", { bg = colors.black, fg = colors.light_gray })
+  set_hl(0, "MatchParen", { fg = colors.lightest_gray, bg = colors.light_gray, bold = true })
+  set_hl(0, "MiniIndentscopeSymbol", { fg = colors.lighter_gray })
+  set_hl(0, "MiniMapSymbolLine", { fg = colors.white })
+  set_hl(0, "MiniMapSymbolView", { fg = colors.light_gray })
+  set_hl(0, "NormalFloat", { bg = colors.black })
+  set_hl(0, "SignColumn", { bg = colors.black })
+  set_hl(0, "Structure", { fg = colors.yellow })
+  set_hl(0, "SpecialChar", { fg = colors.purple })
+  set_hl(0, "TelescopeBorder", { fg = colors.lighter_gray })
 end
