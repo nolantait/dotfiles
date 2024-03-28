ee6c879da05678f2a0ab44afafce1b503f1d11fa
Nolan J Tait <nolanjtait@gmail.com>
2024-03-27 23:01:10 -0700

    Adds db ui

--- config/nvim/lua/plugins/list.lua
+++ config/nvim/lua/plugins/list.lua
@@ -335,6 +335,30 @@ return {
     keys = keymap(require("plugins.keybinds.toggleterm")),
   },
   {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = {
+      {
+        "tpope/vim-dadbod",
+        lazy = true
+      },
+      {
+        "kristijanhusak/vim-dadbod-completion",
+        ft = { "sql", "mysql", "plsql" },
+        lazy = true
+      },
+    },
+    cmd = {
+      "DBUI",
+      "DBUIToggle",
+      "DBUIAddConnection",
+      "DBUIFindBuffer",
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+  },
+  {
     -- Custom fork of mason-update-all which uses notifications instead of print
     "nolantait/mason-update-all",
     cmd = "MasonUpdateAll",
