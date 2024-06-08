28430fa9fe184e5f2d8695dbac80e5901af0ce2d
Nolan J Tait <nolanjtait@gmail.com>
2024-06-08 11:22:40 -0700

    Adds more to colorscheme

--- config/nvim/lua/plugins/configs/colorscheme.lua
+++ config/nvim/lua/plugins/configs/colorscheme.lua
@@ -11,121 +11,141 @@ return function()
   local groups = theme.groups
   local styles = theme.styles
 
+  -- Setup colors
   Color.new("Background", "#16181d")
-
   for name, color in pairs(base16) do
     Color.new(name, color)
   end
 
-  Group.new("Background", colors.lighter_gray, colors.background)
-  Group.new("Normal", colors.lighter_gray, colors.black)
-  Group.new("Cursor", colors.white, colors.black)
-  Group.new("CursorLine", nil, colors.darker_gray)
-  Group.new("Visual", colors.white, colors.dark_gray)
-  Group.new("Directory", colors.blue, nil)
-  Group.new("DiffAdd", colors.green, nil)
-  Group.new("DiffChange", colors.orange, nil)
-  Group.new("DiffDelete", colors.red, nil)
-  Group.new("DiffText", colors.yellow, nil)
-  Group.new("NonText", colors.darker_gray, colors.black)
-  Group.new("Error", colors.red, nil)
-  Group.new("WinSeparator", colors.background, colors.background)
-  Group.new("Folded", colors.light_gray, colors.black)
-  Group.new("LineNr", colors.dark_gray, colors.background)
-  Group.new("Search", colors.black, colors.yellow)
-  Group.new("MatchParen", colors.lightest_gray, colors.light_gray, styles.bold)
-  Group.new("NormalFloat", colors.lighter_gray, colors.darker_gray)
-  Group.new("Title", colors.blue, nil)
-  Group.new("PmenuThumb", colors.lightest_gray, colors.lightest_gray)
-  Group.new("SpellBad", colors.white, nil, styles.undercurl)
-  Group.new("TabLine", colors.gray, colors.darker_gray)
-  Group.new("Special", colors.cyan, nil)
-  Group.new("Constant", colors.orange, nil)
-  Group.new("Function", colors.blue, nil)
-  Group.new("Keyword", colors.purple, nil)
-  Group.new("Comment", colors.gray, nil)
-  Group.new("Info", colors.cyan, nil)
-  Group.new("Todo", colors.yellow, nil, styles.bold)
-  Group.new("Warn", colors.yellow, nil)
-  Group.new("Changed", colors.orange, nil)
-  Group.new("Success", colors.green, nil)
-  Group.new("Identifier", colors.red, nil)
-  Group.new("Label", colors.yellow, nil)
-  Group.new("String", colors.green, nil)
-  Group.new("Delimiter", colors.red:dark(), nil)
-  Group.new("Type", colors.yellow, nil)
-  Group.new("Structure", colors.purple, nil)
-
-  local set_hl = vim.api.nvim_set_hl
-
-  local highlights = {
-    { "Statement",          { link = "Identifier" } },
-    { "Character",          { link = "Identifier" } },
-    { "PreProc",            { link = "Identifier" } },
-    { "@keyword.return",    { link = "Identifier" } },
-    { "@variable",          { link = "Normal" } },
-    { "Number",             { link = "Constant" } },
-    { "DiagnosticInfo",     { link = "Info" } },
-    { "DiagnosticWarn",     { link = "Warn" } },
-    { "DiagnosticError",    { link = "Error" } },
-    { "Removed",            { link = "Error" } },
-    { "Added",              { link = "Success" } },
-    { "ErrorMsg",           { link = "Error" } },
-    { "SpecialChar",        { link = "Character" } },
-    { "Float",              { link = "Number" } },
-    { "NormalNC",           { link = "Normal" } },
-    { "MsgArea",            { link = "Background" } },
-    { "Menu",               { link = "Background" } },
-    { "Scrollbar",          { link = "Background" } },
-    { "NeoTreeNormal",      { link = "Background" } },
-    { "NeoTreeNormalNC",    { link = "Background" } },
-    { "NeoTreeEndOfBuffer", { link = "Background" } },
-    { "Tooltip",            { link = "Float" } },
-    { "lCursor",            { link = "Cursor" } },
-    { "CursorIM",           { link = "Cursor" } },
-    { "TermCursor",         { link = "Cursor" } },
-    { "TermCursorNC",       { link = "Cursor" } },
-    { "CursorColumn",       { link = "CursorLine" } },
-    { "ColorColumn",        { link = "CursorLine" } },
-    { "EndOfBuffer",        { link = "NonText" } },
-    { "IncSearch",          { link = "Search" } },
-    { "Substitute",         { link = "IncSearch" } },
-    { "SignColumn",         { link = "LineNr" } },
-    { "LineNrAbove",        { link = "LineNr" } },
-    { "LineNrBelow",        { link = "LineNr" } },
-    { "CursorLineSign",     { link = "CursorLine" } },
-    { "CursorLineNr",       { link = "CursorLine" } },
-    { "MsgSeparator",       { link = "WinSeparator" } },
-    { "FloatBorder",        { link = "NormalFloat" } },
-    { "FloatTitle",         { link = "Title" } },
-    { "FloatFooter",        { link = "FloatTitle" } },
-    { "Pmenu",              { link = "NormalFloat" } },
-    { "PmenuKind",          { link = "NormalFloat" } },
-    { "PmenuExtra",         { link = "NormalFloat" } },
-    { "PmenuSel",           { link = "Visual" } },
-    { "PmenuKindSel",       { link = "Visual" } },
-    { "PmenuExtraSel",      { link = "Visual" } },
-    { "SnippetTabstop",     { link = "Visual" } },
-    { "PmenuSbar",          { link = "Normal" } },
-    { "QuickFixLine",       { link = "CursorLine" } },
-    { "SpecialKey",         { link = "NonText" } },
-    { "SpellCap",           { link = "SpellBad" } },
-    { "SpellLocal",         { link = "SpellBad" } },
-    { "SpellRare",          { link = "SpellBad" } },
-    { "StatusLine",         { link = "TabLine" } },
-    { "StatusLineNC",       { link = "TabLine" } },
-    { "TabLineFill",        { link = "TabLine" } },
-    { "TabLineSel",         { link = "TabLine" } },
-    { "WinBar",             { link = "TabLine" } },
-    { "WinBarNC",           { link = "TabLine" } },
-    { "Whitespace",         { link = "NonText" } },
-    { "WildMenu",           { link = "Search" } },
+  local highlight_groups = {
+    { "Background",   colors.lighter_gray,  colors.background },
+    { "Normal",       colors.lighter_gray,  colors.black },
+    { "Cursor",       colors.white,         colors.black },
+    { "CursorLine",   nil,                  colors.darker_gray },
+    { "Visual",       nil,                  colors.dark_gray },
+    { "Directory",    colors.blue,          nil },
+    { "DiffAdd",      colors.green,         nil },
+    { "DiffChange",   colors.orange,        nil },
+    { "DiffDelete",   colors.red,           nil },
+    { "DiffText",     colors.yellow,        nil },
+    { "NonText",      colors.darker_gray,   colors.black },
+    { "Error",        colors.red,           nil },
+    { "WinSeparator", colors.background,    colors.background },
+    { "Folded",       colors.light_gray,    colors.black },
+    { "LineNr",       colors.dark_gray,     colors.background },
+    { "Search",       colors.black,         colors.yellow },
+    { "MatchParen",   colors.lightest_gray, colors.light_gray,   styles.bold },
+    { "NormalFloat",  colors.lighter_gray,  colors.black },
+    { "Title",        colors.blue,          nil },
+    { "PmenuThumb",   colors.lightest_gray, colors.lightest_gray },
+    { "TabLine",      colors.gray,          colors.darker_gray },
+    { "Special",      colors.cyan,          nil },
+    { "Constant",     colors.orange,        nil },
+    { "Function",     colors.blue,          nil },
+    { "Keyword",      colors.purple,        nil },
+    { "Comment",      colors.gray,          nil },
+    { "Info",         colors.cyan,          nil },
+    { "Todo",         colors.yellow,        nil,                 styles.bold },
+    { "Warn",         colors.yellow,        nil },
+    { "Changed",      colors.orange,        nil },
+    { "Success",      colors.green,         nil },
+    { "Identifier",   colors.red,           nil },
+    { "Conditional",  colors.purple,        nil },
+    { "Label",        colors.yellow,        nil },
+    { "String",       colors.green,         nil },
+    { "Delimiter",    colors.red,           nil },
+    { "Type",         colors.yellow,        nil },
+    { "Structure",    colors.yellow,        nil },
+    { "SpecialChar",  colors.purple,        nil },
+    { "Deprecated",   nil,                  nil,                 styles.strikethrough },
+    { "Bold",         nil,                  nil,                 styles.bold },
+    { "Underlined",   nil,                  nil,                 styles.underline },
+    { "FloatShadow",  nil,                  colors.background }
   }
 
-  for _, highlight in ipairs(highlights) do
-    set_hl(0, unpack(highlight))
+  local highlights = {
+    { "MiniIndentscopeSymbol",      { link = "Normal" } },
+    { "CmpItemKindKeywordDefault",  { link = "Keyword" } },
+    { "CmpItemKindMethodDefault",   { link = "Function" } },
+    { "CmpItemKindModuleDefault",   { link = "Structure" } },
+    { "CmpItemKindStructDefault",   { link = "Structure" } },
+    { "CmpItemKindPropertyDefault", { link = "Identifier" } },
+    { "CmpItemKindValueDefault",    { link = "Identifier" } },
+    { "AlphaHeader",                { link = "Title" } },
+    { "AlphaFooter",                { link = "Warn" } },
+    { "DiagnosticDeprecated",       { link = "Deprecated" } },
+    { "Statement",                  { link = "Identifier" } },
+    { "Character",                  { link = "Identifier" } },
+    { "PreProc",                    { link = "Identifier" } },
+    { "@keyword.return",            { link = "Identifier" } },
+    { "@variable",                  { link = "Normal" } },
+    { "Number",                     { link = "Constant" } },
+    { "DiagnosticInfo",             { link = "Info" } },
+    { "DiagnosticWarn",             { link = "Warn" } },
+    { "DiagnosticError",            { link = "Error" } },
+    { "Removed",                    { link = "Error" } },
+    { "Added",                      { link = "Success" } },
+    { "ErrorMsg",                   { link = "Error" } },
+    { "Float",                      { link = "Number" } },
+    { "NormalNC",                   { link = "Normal" } },
+    { "MsgArea",                    { link = "Background" } },
+    { "Menu",                       { link = "Background" } },
+    { "Scrollbar",                  { link = "Background" } },
+    { "NeoTreeNormal",              { link = "Background" } },
+    { "NeoTreeNormalNC",            { link = "Background" } },
+    { "NeoTreeEndOfBuffer",         { link = "Background" } },
+    { "Tooltip",                    { link = "Float" } },
+    { "lCursor",                    { link = "Cursor" } },
+    { "CursorIM",                   { link = "Cursor" } },
+    { "TermCursor",                 { link = "Cursor" } },
+    { "TermCursorNC",               { link = "Cursor" } },
+    { "CursorColumn",               { link = "CursorLine" } },
+    { "ColorColumn",                { link = "CursorLine" } },
+    { "EndOfBuffer",                { link = "NonText" } },
+    { "IncSearch",                  { link = "Search" } },
+    { "CurSearch",                  { link = "Search" } },
+    { "SatelliteSearch",            { link = "Search" } },
+    { "Substitute",                 { link = "Search" } },
+    { "SignColumn",                 { link = "LineNr" } },
+    { "LineNrAbove",                { link = "LineNr" } },
+    { "LineNrBelow",                { link = "LineNr" } },
+    { "CursorLineSign",             { link = "CursorLine" } },
+    { "CursorLineNr",               { link = "CursorLine" } },
+    { "MsgSeparator",               { link = "WinSeparator" } },
+    { "FloatBorder",                { link = "NormalFloat" } },
+    { "FloatTitle",                 { link = "Title" } },
+    { "FloatFooter",                { link = "FloatTitle" } },
+    { "Pmenu",                      { link = "NormalFloat" } },
+    { "PmenuKind",                  { link = "NormalFloat" } },
+    { "PmenuExtra",                 { link = "NormalFloat" } },
+    { "PmenuSel",                   { link = "Visual" } },
+    { "PmenuKindSel",               { link = "Visual" } },
+    { "PmenuExtraSel",              { link = "Visual" } },
+    { "SnippetTabstop",             { link = "Visual" } },
+    { "PmenuSbar",                  { link = "Normal" } },
+    { "QuickFixLine",               { link = "CursorLine" } },
+    { "SpecialKey",                 { link = "NonText" } },
+    { "SpellBad",                   { link = "Underlined" } },
+    { "SpellCap",                   { link = "SpellBad" } },
+    { "SpellLocal",                 { link = "SpellBad" } },
+    { "SpellRare",                  { link = "SpellBad" } },
+    { "StatusLine",                 { link = "TabLine" } },
+    { "StatusLineNC",               { link = "TabLine" } },
+    { "TabLineFill",                { link = "TabLine" } },
+    { "TabLineSel",                 { link = "TabLine" } },
+    { "WinBar",                     { link = "TabLine" } },
+    { "WinBarNC",                   { link = "TabLine" } },
+    { "Whitespace",                 { link = "NonText" } },
+    { "WildMenu",                   { link = "Search" } },
+  }
+
+  for _, group in ipairs(highlight_groups) do
+    Group.new(unpack(group))
   end
 
+  for _, highlight in ipairs(highlights) do
+    vim.api.nvim_set_hl(0, unpack(highlight))
+  end
 
   -- background = #16181d
 
@@ -151,8 +171,6 @@ return function()
   --   use_cterm = true,
   --   plugins = { default = true }
   -- })
-
-
 
 
   -- local highlights = {
