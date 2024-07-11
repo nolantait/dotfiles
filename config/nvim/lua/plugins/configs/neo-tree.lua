435da4a8dc8311fd0cb2950cfd0d0aab81561656
Nolan J Tait <nolanjtait@gmail.com>
2024-07-11 13:00:11 -0700

    Fixes neotree

--- config/nvim/lua/plugins/configs/neo-tree.lua
+++ config/nvim/lua/plugins/configs/neo-tree.lua
@@ -10,13 +10,21 @@ return function()
   neotree.setup({
     close_if_last_window = true,
     commands = {
+      parent_or_close = function(state)
+        local node = state.tree:get_node()
+        if node:has_children() and node:is_expanded() then
+          state.commands.toggle_node(state)
+        else
+          require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
+        end
+      end,
       -- https://github.com/AstroNvim/AstroNvim/blob/9054fa4c767f0327340d82411d4e7f10307d9aca/lua/astronvim/plugins/neo-tree.lua#L116C1-L131C13
       child_or_open = function(state)
         local node = state.tree:get_node()
         if node:has_children() then
           if not node:is_expanded() then -- if unexpanded, expand
             state.commands.toggle_node(state)
-          else -- if expanded and has children, seleect the next child
+          else                           -- if expanded and has children, seleect the next child
             if node.type == "file" then
               state.commands.open(state)
             else
@@ -134,5 +142,18 @@ return function()
         require("neo-tree.sources.git_status").refresh()
       end
     end,
+  })
+
+  vim.api.nvim_create_autocmd('BufEnter', {
+    -- make a group to be able to delete it later
+    group = vim.api.nvim_create_augroup('NeoTreeInit', { clear = true }),
+    callback = function()
+      local f = vim.fn.expand('%:p')
+      if vim.fn.isdirectory(f) ~= 0 then
+        vim.cmd('Neotree current dir=' .. f)
+        -- neo-tree is loaded now, delete the init autocmd
+        vim.api.nvim_clear_autocmds { group = 'NeoTreeInit' }
+      end
+    end
   })
 end
