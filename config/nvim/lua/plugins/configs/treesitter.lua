64d22b60e9cb7238e472de628010cd47afa4fde2
Nolan J Tait <nolanjtait@gmail.com>
2024-10-10 20:08:13 -0700

    Re-enables treesitter highlighting

--- config/nvim/lua/plugins/configs/treesitter.lua
+++ config/nvim/lua/plugins/configs/treesitter.lua
@@ -35,7 +35,7 @@ return function()
       enable = true
     },
     highlight = {
-      enable = false, -- false will disable the whole extension
+      enable = true, -- false will disable the whole extension
       additional_vim_regex_highlighting = true,
       use_language_tree = true,
     },
