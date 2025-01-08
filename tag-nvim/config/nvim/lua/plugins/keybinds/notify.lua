0acf42b99ad0be0f20050c7cd7ac911ebe4cecf1
Nolan J Tait <nolanjtait@gmail.com>
2025-01-07 22:02:05 -0800

    Changes dismiss notifications keybind to match snacks

--- tag-nvim/config/nvim/lua/plugins/keybinds/notify.lua
+++ tag-nvim/config/nvim/lua/plugins/keybinds/notify.lua
@@ -1,7 +1,7 @@
 return {
   {
     mode = "n",
-    key = "<Leader>dn",
+    key = "<Leader>un",
     command = "<cmd>lua require('notify').dismiss({silent = true, pending = true})<CR>",
     description = "Dismiss all notifications",
   },
