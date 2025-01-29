505e06c286035795a05c05e700ef8bf25b908b34
Nolan J Tait <nolanjtait@gmail.com>
2025-01-29 14:59:37 -0800

    Gets rid of model name, replaces with config file in each project

--- tag-nvim/config/nvim/lua/plugins/keybinds/aider.lua
+++ tag-nvim/config/nvim/lua/plugins/keybinds/aider.lua
@@ -2,7 +2,7 @@ return {
   {
     mode = "n",
     key = "<Leader>ai",
-    command = "<cmd>AiderOpen --model r1 --no-auto-commits --no-attribute-committer --no-attribute-author<CR>",
+    command = "<cmd>AiderOpen --no-auto-commits --no-attribute-committer --no-attribute-author<CR>",
     description = "Open ai with Aider",
   },
 }
