f55c594985dc726d2abd225722d86d27a2babd59
Nolan J Tait <nolanjtait@gmail.com>
2025-03-22 19:24:30 -0700

    Changes keybind for grug-far

--- tag-nvim/config/nvim/lua/plugins/keybinds/grug-far.lua
+++ tag-nvim/config/nvim/lua/plugins/keybinds/grug-far.lua
@@ -1,13 +1,13 @@
 return {
   {
     mode = "n",
-    key = "<Leader>fr",
+    key = "<Leader>rf",
     command = "<cmd>lua require'grug-far'.open({ transient = true, prefills = { paths = vim.fn.expand('%') } })<CR>",
     description = "Open find and replace in file",
   },
   {
     mode = { "n", "v" },
-    key = "<Leader>fa",
+    key = "<Leader>ra",
     command = "<cmd>lua require'grug-far'.open({ transient = true })<CR>",
     description = "Open find and replace in all files",
   },
