876119866b8fe6b857548c8929617492168ec236
nolantait <nolanjtait@gmail.com>
2025-03-31 11:08:40 -0700

    Refactors mason-lspconfig

--- tag-nvim/config/nvim/lua/plugins/configs/lsp/mason-lspconfig.lua
+++ tag-nvim/config/nvim/lua/plugins/configs/lsp/mason-lspconfig.lua
@@ -10,29 +10,30 @@ M.setup = function(handlers)
     automatic_installation = false,
   })
 
+  local SKIP = {
+    -- Disabling rust_analyzer for the rustaceanvim plugin
+    "rust_analyzer",
+  }
+
   -- Function to load our lsp server setups in plugins.lsp.servers or
   -- fallback to a default
   local function mason_lsp_handler(lsp_name)
-    -- Temporary rename until they move to the new naming
-    if lsp_name == "tsserver" then
-      lsp_name = "ts_ls"
-    end
-
-    if lsp_name == "rust_analyzer" then
+    -- Skip servers that in the list
+    if vim.tbl_contains(SKIP, lsp_name) then
       return
     end
 
+    -- Try to load the server config from "config/nvim/lua/plugins/lsp/servers"
     local ok, custom_handler =
       pcall(require, "plugins.configs.lsp.servers." .. lsp_name)
-    -- Use preset if there is no user definition
+
+    -- Use the default if there is no user definition
     if not ok then
-      -- Default to use factory config for server(s) that doesn't include a spec
       lspconfig[lsp_name].setup(handlers)
       return
     elseif type(custom_handler) == "function" then
       --- Case where language server requires its own setup
       --- Make sure to call require("lspconfig")[lsp_name].setup() in the function
-      --- See `clangd.lua` for example.
       custom_handler(handlers)
     elseif type(custom_handler) == "table" then
       lspconfig[lsp_name].setup(
@@ -40,7 +41,10 @@ M.setup = function(handlers)
       )
     else
       local error_message = string.format(
-        "Failed to setup [%s].\n\nServer definition under `completion/servers` must return\neither a fun(opts) or a table (got '%s' instead)",
+        [[
+          Failed to setup [%s].\n\nServer definition under `completion/servers`
+          must return\neither a fun(opts) or a table (got '%s' instead)
+        ]],
         lsp_name,
         type(custom_handler)
       )
