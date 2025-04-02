3bdc6059be597f43875f34273442a1829a7b6dd4
Nolan J Tait <nolanjtait@gmail.com>
2025-04-01 20:39:22 -0700

    Sets formatters for copilot_cmp explicitly

--- tag-nvim/config/nvim/lua/plugins/configs/cmp/copilot-cmp.lua
+++ tag-nvim/config/nvim/lua/plugins/configs/cmp/copilot-cmp.lua
@@ -23,6 +23,11 @@ M.setup = function()
       method = "getCompletionsCycling",
       event = { "InsertEnter", "LspAttach" },
       fix_pairs = true,
+      formatters = {
+        label = require("copilot_cmp.format").format_label_text,
+        insert_text = require("copilot_cmp.format").format_insert_text,
+        preview = require("copilot_cmp.format").deindent,
+      },
     })
 
     local utils = require("utils.cmp")
