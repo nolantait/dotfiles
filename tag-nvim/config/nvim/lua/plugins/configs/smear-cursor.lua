c558f98a2c82617b6dda33275a5a4630ca840929
Nolan J Tait <nolanjtait@gmail.com>
2025-04-28 08:00:23 -0700

    Increases min smear distance

--- tag-nvim/config/nvim/lua/plugins/configs/smear-cursor.lua
+++ tag-nvim/config/nvim/lua/plugins/configs/smear-cursor.lua
@@ -10,7 +10,9 @@ return function()
 
     -- Smear cursor when moving within line or to neighbor lines.
     -- Use `min_horizontal_distance_smear` and `min_vertical_distance_smear` for finer control
-    smear_between_neighbor_lines = true,
+    smear_between_neighbor_lines = false,
+    min_horizontal_distance_smear = 5,
+    min_vertical_distance_smear = 5,
 
     -- Draw the smear in buffer space instead of screen space when scrolling
     scroll_buffer_space = true,
