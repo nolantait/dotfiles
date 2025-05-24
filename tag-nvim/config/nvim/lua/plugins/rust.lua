3b5528f9a64d597fa2cafa0fbaf315cd1ac59e36
Nolan J Tait <nolanjtait@gmail.com>
2025-05-24 16:28:59 -0700

    Fixes rustacean setup

--- tag-nvim/config/nvim/lua/plugins/rust.lua
+++ tag-nvim/config/nvim/lua/plugins/rust.lua
@@ -1,38 +1,49 @@
 return {
   {
+    "saecki/crates.nvim",
+    event = { "BufRead Cargo.toml" },
+    opts = {
+      completion = {
+        cmp = { enabled = true },
+      },
+    },
+  },
+  {
     "mrcjkb/rustaceanvim",
     version = "^6",
     lazy = false,
-    opts = {
-      tools = {
-        float_win_config = {
-          border = "rounded",
+    config = function()
+      vim.g.rustaceanvim = {
+        tools = {
+          float_win_config = {
+            border = "rounded",
+          },
         },
-      },
-      default_settings = {
-        -- rust-analyzer language server configuration
-        ["rust-analyzer"] = {
-          cargo = {
-            allFeatures = true,
-            loadOutDirsFromCheck = true,
-            runBuildScripts = true,
-          },
-          -- Add clippy lints for Rust.
-          checkOnSave = {
-            allFeatures = true,
-            command = "clippy",
-            extraArgs = { "--no-deps" },
-          },
-          procMacro = {
-            enable = true,
-            ignored = {
-              ["async-trait"] = { "async_trait" },
-              ["napi-derive"] = { "napi" },
-              ["async-recursion"] = { "async_recursion" },
+        default_settings = {
+          -- rust-analyzer language server configuration
+          ["rust-analyzer"] = {
+            cargo = {
+              allFeatures = true,
+              loadOutDirsFromCheck = true,
+              runBuildScripts = true,
+            },
+            -- Add clippy lints for Rust.
+            checkOnSave = {
+              allFeatures = true,
+              command = "clippy",
+              extraArgs = { "--no-deps" },
+            },
+            procMacro = {
+              enable = true,
+              ignored = {
+                ["async-trait"] = { "async_trait" },
+                ["napi-derive"] = { "napi" },
+                ["async-recursion"] = { "async_recursion" },
+              },
             },
           },
         },
-      },
-    },
+      }
+    end,
   },
 }
