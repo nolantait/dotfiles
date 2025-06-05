97f27e54a49c523cbe6bb7531f4337794725c4f3
Nolan J Tait <nolanjtait@gmail.com>
2025-06-05 12:04:27 -0700

    Fixes avante setup

--- tag-nvim/config/nvim/lua/plugins/avante.lua
+++ tag-nvim/config/nvim/lua/plugins/avante.lua
@@ -46,20 +46,35 @@ return {
       file_selector = {
         provider = "telescope",
       },
-      openai = {
-        endpoint = "https://api.openai.com/v1",
-        model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-        timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-        temperature = 0,
-        max_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-        --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
-      },
-      claude = {
-        endpoint = "https://api.anthropic.com",
-        model = "claude-3-7-sonnet-latest",
-        temperature = 0,
-        max_tokens = 4096,
-        disable_tools = false,
+      providers = {
+        openai = {
+          endpoint = "https://api.openai.com/v1",
+          model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
+          timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+          extra_request_body = {
+            max_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+            reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+            options = {
+              temperature = 0,
+              num_ctx = 20480,
+              keep_alive = "5m"
+            }
+          },
+        },
+        claude = {
+          endpoint = "https://api.anthropic.com",
+          model = "claude-3-7-sonnet-latest",
+          disable_tools = false,
+          extra_request_body = {
+            reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+            max_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+            options = {
+              temperature = 0,
+              num_ctx = 20480,
+              keep_alive = "5m"
+            }
+          },
+        },
       },
       mappings = {
         submit = {
