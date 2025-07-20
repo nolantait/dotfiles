fdb179927c3c745d4aa9754f1dccb5e58c32a9a4
Nolan J Tait <nolanjtait@gmail.com>
2025-07-20 12:19:17 -0700

    Improves zsh file listing

--- zsh/configs/post/completion.zsh
+++ zsh/configs/post/completion.zsh
@@ -12,8 +12,8 @@ fi;
 # --- Global completion behavior ---
 # Enable completions
 zstyle ':completion:*' menu select=2
-# Match uppercase from lowercase
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+# Match case-insensitively, also match partial words
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 zstyle ':completion:*' group-name ''
 # Automatically find new executables in path
 zstyle ':completion:*' rehash true
@@ -21,20 +21,22 @@ zstyle ':completion:*' rehash true
 zstyle ':completion:*' insert-tab pending
 # Speed up completions
 zstyle ':completion:*' use-cache true
-zstyle ':completion:*' cache-path ~/.cache/zcache
+zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zcache"
 zstyle ':completion:*' completer _complete _match _approximate
 zstyle ':completion:*' select-prompt '%SScrolling active: current selection at %p%s'
 
 # --- Usability improvements ---
+zstyle ':completion:*' complete-options true
+zstyle ':completion:*' file-list all
 # `cd` will never select the parent directory (e.g.: cd ../<TAB>)
 zstyle ':completion:*:cd:*' ignore-parents parent pwd
 # automatically complete 'cd -<tab>' and 'cd -<ctrl-d>' with menu
-zstyle ':completion:*:*:cd:*:directory-stack' menu yes select
+zstyle ':completion:*:*:cd:*:directory-stack' menu true select
 zstyle ':completion:*:match:*' original only
 # Allow one error for every three characters typed in approximate completer
 zstyle ':completion:*:approximate:' max-errors 'reply=( $((($#PREFIX+$#SUFFIX)/3 )) numeric )'
 # Separate matches into groups
-zstyle ':completion:*:matches' group 'yes'
+zstyle ':completion:*:matches' group true
 # If you end up using a directory as argument, this will remove the trailing
 # slash (useful in ln)
 zstyle ':completion:*' squeeze-slashes true
@@ -49,18 +51,18 @@ zstyle ':completion:*:correct:*' insert-unambiguous true
 zstyle ':completion:*:corrections' format $'%{\e[0;31m%}%d (errors: %e)%{\e[0m%}'
 zstyle ':completion:*:correct:*' original true
 # Describe options in full
-zstyle ':completion:*:options' description 'yes'
+zstyle ':completion:*:options' description true
 # complete manual by their section
 zstyle ':completion:*:manuals' separate-sections true
 zstyle ':completion:*:manuals.*' insert-sections   true
-zstyle ':completion:*:man:*' menu yes select
+zstyle ':completion:*:man:*' menu true select
 # Define files to ignore for zcompile
-zstyle ':completion:*:*:zcompile:*'    ignored-patterns '(*~|*.zwc)'
-zstyle ':completion:correct:'          prompt 'correct to: %e'
+zstyle ':completion:*:*:zcompile:*' ignored-patterns '(*~|*.zwc)'
+zstyle ':completion:correct:' prompt 'correct to: %e'
 # Insert all expansions for expand completer
 zstyle ':completion:*:expand:*' tag-order all-expansions
 zstyle ':completion:*:history-words' list false
-zstyle ':completion:*:history-words' menu yes
+zstyle ':completion:*:history-words' menu true
 # Ignore completion functions for commands you don’t have:
 zstyle ':completion:*:functions' ignored-patterns '_*'
 zstyle ':completion:*:messages' format '%d'
@@ -71,7 +73,7 @@ zstyle ':completion:*:processes' command 'ps -au$USER'
 # Provide more processes in completion of programs like killall:
 zstyle ':completion:*:processes-names' command 'ps c -u ${USER} -o command | uniq'
 zstyle ':completion:*:kill:*' command 'ps -u $(whoami) -o pid,%cpu,tty,cputime,cmd'
-zstyle ':completion:*:*:killall:*' menu yes select
+zstyle ':completion:*:*:killall:*' menu true select
 zstyle ':completion:*:killall:*' force-list always
 # Command for process lists, the local web server details and host completion
 zstyle ':completion:*:urls' local 'www' '/var/www/' 'public_html'
