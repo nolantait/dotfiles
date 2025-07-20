8d76b6da5417c3269be3c504be1e7619306e5981
Nolan J Tait <nolanjtait@gmail.com>
2025-07-20 12:06:19 -0700

    Improves zsh completions

--- zsh/configs/post/completion.zsh
+++ zsh/configs/post/completion.zsh
@@ -6,16 +6,15 @@ autoload -Uz compinit
 if [[ -n $HOME/.zcompdump(#qN.mh+24) ]]; then
   compinit -d $HOME/.zcompdump;
 else
-  compinit -C;
+  compinit;
 fi;
 
+# --- Global completion behavior ---
 # Enable completions
 zstyle ':completion:*' menu select=2
 # Match uppercase from lowercase
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
-# Separate matches into groups
-zstyle ':completion:*:matches'         group 'yes'
-zstyle ':completion:*'                 group-name ''
+zstyle ':completion:*' group-name ''
 # Automatically find new executables in path
 zstyle ':completion:*' rehash true
 # Pasting with tabs doesn't perform completion
@@ -25,12 +24,8 @@ zstyle ':completion:*' use-cache true
 zstyle ':completion:*' cache-path ~/.cache/zcache
 zstyle ':completion:*' completer _complete _match _approximate
 zstyle ':completion:*' select-prompt '%SScrolling active: current selection at %p%s'
-# If you end up using a directory as argument, this will remove the trailing
-# slash (useful in ln)
-zstyle ':completion:*' squeeze-slashes true
 
-# activate color-completion
-zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+# --- Usability improvements ---
 # `cd` will never select the parent directory (e.g.: cd ../<TAB>)
 zstyle ':completion:*:cd:*' ignore-parents parent pwd
 # automatically complete 'cd -<tab>' and 'cd -<ctrl-d>' with menu
@@ -38,10 +33,27 @@ zstyle ':completion:*:*:cd:*:directory-stack' menu yes select
 zstyle ':completion:*:match:*' original only
 # Allow one error for every three characters typed in approximate completer
 zstyle ':completion:*:approximate:' max-errors 'reply=( $((($#PREFIX+$#SUFFIX)/3 )) numeric )'
+# Separate matches into groups
+zstyle ':completion:*:matches' group 'yes'
+# If you end up using a directory as argument, this will remove the trailing
+# slash (useful in ln)
+zstyle ':completion:*' squeeze-slashes true
+
+# --- Description formatting and UI ---
+# activate color-completion
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+# format on completion
+zstyle ':completion:*:descriptions' format $'%{\e[0;31m%}completing %B%d%b%{\e[0m%}'
 # Start menu completion only if it could find no unambiguous initial string
 zstyle ':completion:*:correct:*' insert-unambiguous true
 zstyle ':completion:*:corrections' format $'%{\e[0;31m%}%d (errors: %e)%{\e[0m%}'
 zstyle ':completion:*:correct:*' original true
+# Describe options in full
+zstyle ':completion:*:options' description 'yes'
+# complete manual by their section
+zstyle ':completion:*:manuals' separate-sections true
+zstyle ':completion:*:manuals.*' insert-sections   true
+zstyle ':completion:*:man:*' menu yes select
 # Define files to ignore for zcompile
 zstyle ':completion:*:*:zcompile:*'    ignored-patterns '(*~|*.zwc)'
 zstyle ':completion:correct:'          prompt 'correct to: %e'
@@ -51,16 +63,8 @@ zstyle ':completion:*:history-words' list false
 zstyle ':completion:*:history-words' menu yes
 # Ignore completion functions for commands you don’t have:
 zstyle ':completion:*:functions' ignored-patterns '_*'
-# format on completion
-zstyle ':completion:*:descriptions' format $'%{\e[0;31m%}completing %B%d%b%{\e[0m%}'
 zstyle ':completion:*:messages' format '%d'
 zstyle ':completion:*:options' auto-description '%d'
-# Describe options in full
-zstyle ':completion:*:options' description 'yes'
-# complete manual by their section
-zstyle ':completion:*:manuals' separate-sections true
-zstyle ':completion:*:manuals.*' insert-sections   true
-zstyle ':completion:*:man:*' menu yes select
 zstyle ':completion:*:paths' accept-exact '*(N)'
 # On processes completion complete all user processes
 zstyle ':completion:*:processes' command 'ps -au$USER'
@@ -71,7 +75,6 @@ zstyle ':completion:*:*:killall:*' menu yes select
 zstyle ':completion:*:killall:*' force-list always
 # Command for process lists, the local web server details and host completion
 zstyle ':completion:*:urls' local 'www' '/var/www/' 'public_html'
-
 # Search path for sudo completion
 zstyle ':completion:*:sudo:*' command-path /usr/local/sbin \
                                            /usr/local/bin  \
@@ -106,10 +109,6 @@ hosts=(
     localhost
 )
 zstyle ':completion:*:hosts' hosts $hosts
-
-# disable zsh bundled function mtools command mcd
-# which causes a conflict.
-compdef -d mcd
 
 # automatically load bash completion functions
 autoload -U +X bashcompinit && bashcompinit
