bc56677992c37d4e868956a478dbf1a3e776bd66
Nolan J Tait <nolanjtait@gmail.com>
2025-08-28 10:50:28 -0700

    Adds additional projections for rails

--- tag-nvim/config/nvim/lua/plugins/ruby.lua
+++ tag-nvim/config/nvim/lua/plugins/ruby.lua
@@ -1,5 +1,38 @@
 -- DOCS: Ruby plugins for Neovim
 
+vim.g.rails_projections = {
+  ["packages/*"] = {
+    ["app/commands/*.rb"] = {
+      alternate = { "spec/commands/{}_spec.rb", "test/commands/{}_test.rb" },
+      type = "command",
+      template = {
+        "class {camelcase|capitalize|colons}",
+        "  def call",
+        "  end",
+        "end",
+      },
+    },
+    ["spec/commands/*_spec.rb"] = {
+      alternate = "app/commands/{}.rb",
+      type = "test",
+    },
+    ["app/actions/*.rb"] = {
+      alternate = { "spec/actions/{}_spec.rb", "test/actions/{}_test.rb" },
+      type = "command",
+      template = {
+        "class {camelcase|capitalize|colons}",
+        "  def call",
+        "  end",
+        "end",
+      },
+    },
+    ["spec/actions/*_spec.rb"] = {
+      alternate = "app/actions/{}.rb",
+      type = "test",
+    },
+  },
+}
+
 return {
   {
     "tpope/vim-rails",
