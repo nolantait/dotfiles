70c44188fdf976091af0e3d12679498bf56219a9
Nolan J Tait <nolanjtait@gmail.com>
2025-08-29 23:16:28 -0700

    Improves coloring of bufferline, curse of devicons

--- tag-nvim/config/nvim/lua/plugins/bufferline.lua
+++ tag-nvim/config/nvim/lua/plugins/bufferline.lua
@@ -21,54 +21,59 @@ return {
     -- Buffer tabs at the top of screen
     "akinsho/bufferline.nvim",
     event = "LazyFile",
-    opts = {
-      options = {
-        always_show_bufferline = true,
-        auto_toggle_bufferline = false,
-        buffer_close_icon = icons.cross,
-        close_command = close_command, -- can be a string | function, see "Mouse actions"
-        close_icon = icons.cross,
-        color_icons = true,
-        diagnostics = "nvim_lsp", -- | "nvim_lsp" | "coc",
-        diagnostics_update_in_insert = false,
-        enforce_regular_tabs = false,
-        hover = {
-          enabled = true,
-          delay = 50,
-          reveal = { "close" },
-        },
-        left_mouse_command = "buffer %d", -- can be a string | function, see "Mouse actions"
-        left_trunc_marker = icons.arrow_left,
-        max_name_length = 14,
-        max_prefix_length = 8, -- prefix used when a buffer is de-duplicated
-        middle_mouse_command = nil, -- can be a string | function, see "Mouse actions"
-        mode = "buffers",
-        modified_icon = icons.circle,
-        numbers = "none", -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
-        offsets = {
-          {
-            filetype = "NvimTree",
-            text = "File Explorer",
-            padding = 1,
-            text_align = "center",
+    config = function()
+      local bufferline = require("bufferline")
+
+      bufferline.setup({
+        options = {
+          always_show_bufferline = true,
+          auto_toggle_bufferline = false,
+          buffer_close_icon = icons.cross,
+          close_command = close_command, -- can be a string | function, see "Mouse actions"
+          close_icon = icons.cross,
+          color_icons = true,
+          diagnostics = "nvim_lsp", -- | "nvim_lsp" | "coc",
+          diagnostics_update_in_insert = false,
+          enforce_regular_tabs = false,
+          hover = {
+            enabled = true,
+            delay = 50,
+            reveal = { "close" },
           },
-          {
-            filetype = "neo-tree",
-            text = "File Explorer",
-            padding = 1,
-            text_align = "center",
+          left_mouse_command = "buffer %d", -- can be a string | function, see "Mouse actions"
+          left_trunc_marker = icons.arrow_left,
+          max_name_length = 14,
+          max_prefix_length = 8, -- prefix used when a buffer is de-duplicated
+          middle_mouse_command = nil, -- can be a string | function, see "Mouse actions"
+          mode = "buffers",
+          modified_icon = icons.circle,
+          numbers = "none", -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
+          offsets = {
+            {
+              filetype = "NvimTree",
+              text = "File Explorer",
+              padding = 1,
+              text_align = "center",
+            },
+            {
+              filetype = "neo-tree",
+              text = "File Explorer",
+              padding = 1,
+              text_align = "center",
+            },
           },
+          right_mouse_command = close_command, -- can be a string | function, see "Mouse actions"
+          right_trunc_marker = icons.arrow_right,
+          separator_style = "thin", -- | "thick" | "thin" | { 'any', 'any' },
+          style_preset = bufferline.style_preset.minimal,
+          show_buffer_icons = true,
+          show_buffer_close_icons = true,
+          show_tab_indicators = false,
+          sort_by = "insert_at_end",
+          tab_size = 20,
+          themable = true,
         },
-        right_mouse_command = close_command, -- can be a string | function, see "Mouse actions"
-        right_trunc_marker = icons.arrow_right,
-        separator_style = "thin", -- | "thick" | "thin" | { 'any', 'any' },
-        show_buffer_icons = true,
-        show_buffer_close_icons = true,
-        show_tab_indicators = false,
-        sort_by = "insert_at_end",
-        tab_size = 20,
-        themable = true,
-      },
-    },
+      })
+    end,
   },
 }
