2f456feb65e615208bc96293a1eb66187074d292
Nolan J Tait <nolanjtait@gmail.com>
2025-09-14 10:53:49 -0700

    Removes comments from blink setup

--- tag-nvim/config/nvim/lua/plugins/blink.lua
+++ tag-nvim/config/nvim/lua/plugins/blink.lua
@@ -2,46 +2,20 @@ return {
   {
     "saghen/blink.cmp",
     event = "LazyFile",
-    -- optional: provides snippets for the snippet source
-    dependencies = {
-      "giuxtaposition/blink-cmp-copilot",
-      -- "rafamadriz/friendly-snippets"
-    },
-
-    -- use a release tag to download pre-built binaries
+    dependencies = { "giuxtaposition/blink-cmp-copilot" },
     version = "1.*",
-    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-    -- build = 'cargo build --release',
-    -- If you use nix, you can build from source using latest nightly rust with:
-    -- build = 'nix run .#build-plugin',
 
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
-      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-      -- 'super-tab' for mappings similar to vscode (tab to accept)
-      -- 'enter' for enter to accept
-      -- 'none' for no mappings
-      --
-      -- All presets have the following mappings:
-      -- C-space: Open menu or open docs if already open
-      -- C-n/C-p or Up/Down: Select next/previous item
-      -- C-e: Hide menu
-      -- C-k: Toggle signature help (if signature.enabled = true)
-      --
-      -- See :h blink-cmp-config-keymap for defining your own keymap
       keymap = {
         preset = "enter",
         ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
         ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
       },
-
-      appearance = {
-        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-        -- Adjusts spacing to ensure icons are aligned
-        nerd_font_variant = "mono",
-      },
-
+      appearance = { nerd_font_variant = "mono" },
+      signature = { enabled = true },
+      fuzzy = { implementation = "rust" },
       completion = {
         keyword = { range = "full" },
         documentation = { auto_show = true },
@@ -84,15 +58,6 @@ return {
           },
         },
       },
-
-      signature = { enabled = true },
-
-      -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-      --
-      -- See the fuzzy documentation for more information
-      fuzzy = { implementation = "rust" },
     },
     opts_extend = { "sources.default" },
   },
