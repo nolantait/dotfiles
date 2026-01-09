e6428ed2920698a928cf0d8284bbd89141200f32
Nolan J Tait <nolanjtait@gmail.com>
2026-01-09 13:06:48 -0800

    Adds typescript tools, bumps mise versions for node and ruby

---
+++ tag-nvim/config/nvim/lua/plugins/typescript-tools.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "pmizio/typescript-tools.nvim",
+    branch = "master",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    opts = {},
+    ft = { "typescript", "typescriptreact", "typescript.tsx" },
+  },
+}
