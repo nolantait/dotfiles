777c62f8e7197dc228a334d0f52c50a7825f2f10
Nolan J Tait <nolanjtait@gmail.com>
2026-06-10 22:53:20 -0700

    Cleans up file location script

--- tag-nvim/config/nvim/lua/components/file_location.lua
+++ tag-nvim/config/nvim/lua/components/file_location.lua
@@ -2,6 +2,7 @@ return function()
   local cursorline = vim.fn.line(".")
   local cursorcol = vim.fn.virtcol(".")
   local filelines = vim.fn.line("$")
+
   local position
   if cursorline == 1 then
     position = "Top"
@@ -11,5 +12,6 @@ return function()
     position =
       string.format("%2d%%%%", math.floor(cursorline / filelines * 100))
   end
+
   return string.format("%s%3d:%-2d", position, cursorline, cursorcol)
 end
